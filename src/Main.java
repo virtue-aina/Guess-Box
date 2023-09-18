04df039cccbb657c6e88f163125e3a45ab89e761
William Imoukhuede <31789392+willingtoeatyam@users.noreply.github.com>
2023-09-18 11:24:59 +0100

    cleaner code + suggestions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
-import static java.lang.System.out;
 
+import static java.lang.System.out;
+//create menu. include "0 to exit"
+//add multiple games. maybe different kind of guessing scenarios
+//quit at any point
+//maybe add saved history (FileReader like Nonogram. Create  [if none] and then update after every game)
 public class Main {
     public static void main(String[] args) {
         guessANumber();
@@ -12,64 +16,42 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Random rando = new Random();
 
-
         out.println("Hi, Please choose a limit to the numbers you want to guess, greater than zero ");
         Scanner limitScn = new Scanner(System.in);
         int limit = limitScn.nextInt();
         int randomNumber = rando.nextInt(limit);
-        boolean gameOn = true;
 
-        while (gameOn) {
-
-            out.println("Hi again, Guess a number from zero to your set limit ");
+        while(limit > 1){
+            out.println("Hi again, Please guess a number between zero and your set limit ");
             int guessedNumber = sc.nextInt();
-            if (guessedNumber < limit) {
-                if (randomNumber > guessedNumber) {
-                    out.println("Close! Hint: It's higher!");
-                } else if (randomNumber < guessedNumber) {
-                    out.println(" Try again, it is lower!");
-                } else  {
-                    System.out.println("You are Correct! ");
-                    gameOn = false;
-                }
+
+            if (randomNumber == guessedNumber) {
+                out.println("Correct! The answer is " + randomNumber + " ,great job!");
             } else {
-                out.println("Please guess number within the limits! ");
-
+                out.println("The number is " + randomNumber + ". Try again, please! ");
             }
-
         }
 
+        out.println("Hi again, Please guess a number between zero and your set limit ");
+        int guessedNumber = sc.nextInt();
+
+        if (guessedNumber < limit ) {
+            if (randomNumber == guessedNumber) {
+                out.println("Correct! The answer is " + randomNumber + " ,great job!");
+            } else {
+                out.println("The number is " + randomNumber + ". Try again, please! ");
+            }
+        }else {
+            out.println("Please guess number within the limits! ");
+        }
     }
-
-
-//    String proceed = sc.next("yes");
-//    String stop = sc.next("no");
-//    System.out.println("Would you like to restart game? yes or no ");
-    // proceed;
-    // stop;
-
-    // if (proceed){}
-    // else{}
-
-//    System.out.println("");
-//     int proximity = Math.abs(randomNumber - guessedNumber);
-    //     String str = str.equalsIgnoreCase();
-    // LinkedList<String> valueList = new LinkedList<String>();
-    //  add.ValueList(guessedNumber);
-    //    lastGuessedNumber = valueList.getLast();
-    //
-
-
-    //newGuessedValue = guessedValue
-    //check if the newguessed value is close to random number
-
 }
 
 
 
-
-
-
-
+// as long as input is below limit
+//accept input compare with correct answer
+//if wrong ask user to try again and start from the beginning
+// if user is correct congratulate user!
 
 
