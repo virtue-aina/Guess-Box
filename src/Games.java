586c296f0b50dd03ed335c6ce3b423c189070adb
virtue-aina <115047899+virtue-aina@users.noreply.github.com>
2023-09-18 12:12:55 +0100

    5th

--- src/Games.java
+++ src/Games.java
@@ -1,2 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
 public class Games {
+
+
+    public static void guessANumber() {
+
+        Scanner sc = new Scanner(System.in);
+        Random rando = new Random();
+
+        out.println("Hi, Please choose a limit to the numbers you want to guess, greater than zero ");
+
+        int limit = sc.nextInt();
+        int randomNumber = rando.nextInt(limit);
+        //boolean gameRunning = false;
+
+        while(){
+            out.println("Hi again, Please guess a number between zero and your set limit ");
+            int guessedNumber = sc.nextInt();
+
+            if (randomNumber == guessedNumber) {
+                out.println("Correct! The answer is " + randomNumber + " ,great job!");
+            } else {
+                out.println("The number is " + randomNumber + ". Try again, please! ");
+            }
+
+            else  if (){
+
+            }
+        }
+
+        out.println("Hi again, Please guess a number between zero and your set limit ");
+        int guessedNumber = sc.nextInt();
+
+        if (guessedNumber < limit ) {
+            if (randomNumber == guessedNumber) {
+                out.println("Correct! The answer is " + randomNumber + " ,great job!");
+            } else {
+                out.println("The number is " + randomNumber + ". Try again, please! ");
+            }
+        }else {
+            out.println("Please guess number within the limits! ");
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.Random;
-import java.util.Scanner;
-import static java.lang.System.out;
-
 //create menu. include "0 to exit"
 //add multiple games. maybe different kind of guessing scenarios
 //number game
@@ -14,43 +10,10 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        guessANumber();
+         Games.guessANumber();
     }
 
-    public static void guessANumber() {
 
-        Scanner sc = new Scanner(System.in);
-        Random rando = new Random();
-
-        out.println("Hi, Please choose a limit to the numbers you want to guess, greater than zero ");
-        Scanner limitScn = new Scanner(System.in);
-        int limit = limitScn.nextInt();
-        int randomNumber = rando.nextInt(limit);
-
-        while(limit > 1){
-            out.println("Hi again, Please guess a number between zero and your set limit ");
-            int guessedNumber = sc.nextInt();
-
-            if (randomNumber == guessedNumber) {
-                out.println("Correct! The answer is " + randomNumber + " ,great job!");
-            } else {
-                out.println("The number is " + randomNumber + ". Try again, please! ");
-            }
-        }
-
-        out.println("Hi again, Please guess a number between zero and your set limit ");
-        int guessedNumber = sc.nextInt();
-
-        if (guessedNumber < limit ) {
-            if (randomNumber == guessedNumber) {
-                out.println("Correct! The answer is " + randomNumber + " ,great job!");
-            } else {
-                out.println("The number is " + randomNumber + ". Try again, please! ");
-            }
-        }else {
-            out.println("Please guess number within the limits! ");
-        }
-    }
 }
 
 
